3ef9d2e6e9684c9d20e4c7500ed97106441918ee
K-AndrewM <andrewmagtibay21@gmail.com>
2025-06-18 22:12:05 +0800

    feat: Add project name & status filter function

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,9 +1,28 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import Pagination from "@/Components/Paginate";
-import { Head, Link } from "@inertiajs/react";
+import { Head, Link, router } from "@inertiajs/react";
 import { PROJECT_STATUS_TEXT_MAP, PROJECT_STATUS_CLASS_MAP } from "@/constants";
+import TextInput from "@/Components/TextInput";
+import SelectInput from "@/Components/SelectInput";
 
-export default function Index({ auth, projects }) {
+export default function Index({ auth, projects, queryParams = null }) {
+    queryParams = queryParams || {};
+    const searchFieldChanged = (name, value) => {
+        if (value) {
+            queryParams[name] = value;
+        } else {
+            delete queryParams[name];
+        }
+
+        router.get(route("project.index"), queryParams);
+    };
+
+    const onKeyPress = (name, e) => {
+        if (e.key !== "Enter") return;
+
+        searchFieldChanged(name, e.target.value);
+    };
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -32,72 +51,136 @@ export default function Index({ auth, projects }) {
                                         <th className="p-3">Actions</th>
                                     </tr>
                                 </thead>
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                    <tr className="text-nowrap">
+                                        <th className="p-3"></th>
+                                        <th className="p-3"></th>
+                                        <th className="p-3">
+                                            <TextInput
+                                                className="w-full"
+                                                defaultValue={queryParams.name}
+                                                placeholder="Project Name"
+                                                onBlur={(e) =>
+                                                    searchFieldChanged(
+                                                        "name",
+                                                        e.target.value
+                                                    )
+                                                }
+                                                onKeyPress={(e) =>
+                                                    onKeyPress("name", e)
+                                                }
+                                            />
+                                        </th>
+                                        <th className="p-3">
+                                            <SelectInput
+                                                className="w-full"
+                                                defaultValue={
+                                                    queryParams.status
+                                                }
+                                                onChange={(e) =>
+                                                    searchFieldChanged(
+                                                        "status",
+                                                        e.target.value
+                                                    )
+                                                }
+                                            >
+                                                <option value="">
+                                                    Select status
+                                                </option>
+                                                <option value="pending">
+                                                    Pending
+                                                </option>
+                                                <option value="in_progress">
+                                                    In Progress
+                                                </option>
+                                                <option value="completed">
+                                                    Completed
+                                                </option>
+                                            </SelectInput>
+                                        </th>
+                                        <th className="p-3"></th>
+                                        <th className="p-3"></th>
+                                        <th className="p-3"></th>
+                                        <th className="p-3"></th>
+                                    </tr>
+                                </thead>
                                 <tbody>
-                                    {projects.data.map((project) => (
-                                        <tr
-                                            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-                                            key={project.id}
-                                        >
-                                            <td className="px-3 py-2">
-                                                {project.id}
-                                            </td>
-                                            <td className="px-3 py-2">
-                                                <img
-                                                    src={project.image_path}
-                                                    alt=""
-                                                    width={{ width: 60 }}
-                                                />
-                                            </td>
-                                            <td className="px-3 py-2">
-                                                {project.name}
-                                            </td>
-                                            <td className="px-3 py-2 ">
-                                                <span
-                                                    className={
-                                                        "px-2 py-1 rounded text-white " +
-                                                        PROJECT_STATUS_CLASS_MAP[
-                                                            project.status
-                                                        ]
-                                                    }
-                                                >
-                                                    {
-                                                        PROJECT_STATUS_TEXT_MAP[
-                                                            project.status
-                                                        ]
-                                                    }
-                                                </span>
-                                            </td>
-                                            <td className="px-3 py-2 text-nowrap">
-                                                {project.created_at}
-                                            </td>
-                                            <td className="px-3 py-2 text-nowrap">
-                                                {project.due_date}
-                                            </td>
-                                            <td className="px-3 py-2">
-                                                {project.createdBy.name}
-                                            </td>
-                                            <td className="px-3 py-2">
-                                                <Link
-                                                    href={route(
-                                                        "project.edit",
-                                                        project.id
-                                                    )}
-                                                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
-                                                >
-                                                    Edit
-                                                </Link>
-                                                <Link
-                                                    href={route(
-                                                        "project.destroy",
-                                                        project.id
-                                                    )}
-                                                    className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
-                                                >
-                                                    Delete
-                                                </Link>
+                                    {projects.data.length > 0 ? (
+                                        projects.data.map((project) => (
+                                            <tr
+                                                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                                                key={project.id}
+                                            >
+                                                <td className="px-3 py-2">
+                                                    {project.id}
+                                                </td>
+                                                <td className="px-3 py-2">
+                                                    <img
+                                                        src={project.image_path}
+                                                        alt=""
+                                                        width={{ width: 60 }}
+                                                    />
+                                                </td>
+                                                <td className="px-3 py-2">
+                                                    {project.name}
+                                                </td>
+                                                <td className="px-3 py-2 ">
+                                                    <span
+                                                        className={
+                                                            "px-2 py-1 rounded text-white " +
+                                                            PROJECT_STATUS_CLASS_MAP[
+                                                                project.status
+                                                            ]
+                                                        }
+                                                    >
+                                                        {
+                                                            PROJECT_STATUS_TEXT_MAP[
+                                                                project.status
+                                                            ]
+                                                        }
+                                                    </span>
+                                                </td>
+                                                <td className="px-3 py-2 text-nowrap">
+                                                    {project.created_at}
+                                                </td>
+                                                <td className="px-3 py-2 text-nowrap">
+                                                    {project.due_date}
+                                                </td>
+                                                <td className="px-3 py-2">
+                                                    {project.createdBy.name}
+                                                </td>
+                                                <td className="px-3 py-2">
+                                                    <Link
+                                                        href={route(
+                                                            "project.edit",
+                                                            project.id
+                                                        )}
+                                                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
+                                                    >
+                                                        Edit
+                                                    </Link>
+                                                    <Link
+                                                        href={route(
+                                                            "project.destroy",
+                                                            project.id
+                                                        )}
+                                                        className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
+                                                    >
+                                                        Delete
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    ) : (
+                                        <tr>
+                                            <td
+                                                colSpan="8"
+                                                className="text-center py-4"
+                                            >
+                                                No data available
                                             </td>
                                         </tr>
-                                    ))}
+                                    )}
                                 </tbody>
                             </table>
                             <Pagination links={projects.meta.links} />
