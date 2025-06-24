8f1b4ff68f206d94c0bc4783904af02e7fcf5e61
K-AndrewM <andrewmagtibay21@gmail.com>
2025-06-24 21:42:45 +0800

    feat: Create backend and controller with full CRUD for User
    - Populate index, create, store, edit, update, and destroy functions
    - Add email_verified_at as to fillable for User to verify users created via CRUD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,31 +5,51 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
+    public function index(){
+        $query = User::query();
+
+        if (request('name')) {
+            $query->where('name', 'like', '%' . request('name') . '%');
+        }
+        if (request('email')) {
+            $query->where('email', 'like', '%'.request('email').'%');
+        }
+        $sortField = request('sort_field', 'created_at');
+        $sortDirection = request('sort_direction', 'desc');
+
+        $users = $query->orderBy($sortField, $sortDirection)->paginate(10);
+
+        return inertia('User/Index', [
+            'users' => UserResource::collection($users),
+            'queryParams' => request()->query() ?: ['sort_field' => 'created_at', 'sort_direction' => 'desc'],
+            'success' => session('success'),
+        ]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
+    public function create(){
+        return inertia('User/Create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreUserRequest $request)
-    {
-        //
+    public function store(StoreUserRequest $request){
+        $data = $request->validated();
+        $data['password'] = bcrypt($data['password']);
+        $data['email_verified_at'] = time();
+        User::create($data);
+
+        return to_route('user.index')->with('success', 'User was created');
     }
 
     /**
@@ -43,24 +63,35 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(User $user)
-    {
-        //
+    public function edit(User $user){
+        return inertia('User/Edit', [
+            'user' => new UserResource($user),
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateUserRequest $request, User $user)
-    {
-        //
+    public function update(UpdateUserRequest $request, User $user){
+        $data = $request->validated();
+        $password = $data['password'] ?? null;
+        if($password) {
+            $data['password'] = bcrypt($data['password']);
+        } else {
+            unset($data['password']);
+        }
+        $user->update($data);
+
+        return to_route('user.index')->with('success', "User {$user->name} was updated");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User $user)
-    {
-        //
+    public function destroy(User $user){
+        $userName = $user->name;
+        $user->delete();
+
+        return to_route('user.index')->with('success', "User {$userName} was deleted");
     }
 }
